c65d17c87357e61369adc4c90bd1bc471919b54c
Prototype-N <nazar_d@meta.ua>
2016-02-28 12:43:54 +0200

    first commit

---
+++ Test/src/test/Test.java
@@ -0,0 +1,42 @@
+package test;
+import java.util.Scanner;
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+	        int firstOperand, secondOperand;
+	        char operation;
+	 
+	        System.out.println("Enter 1st operand:");
+	        firstOperand = sc.nextInt();
+	 
+	        System.out.println("Enter 2nd operand:");
+	        secondOperand = sc.nextInt();
+	 
+	        System.out.println("Enter operation type:");
+	        operation = sc.next().charAt(0);
+	 
+	        switch (operation) {
+	            case '*': {
+	                System.out.printf("%d * %d = %d", firstOperand, secondOperand, firstOperand * secondOperand);
+	                break;
+	            }
+	            case '/': {
+	                System.out.printf("%d / %d = %d", firstOperand, secondOperand, firstOperand / secondOperand);
+	                break;
+	            }
+	            case '+': {
+	                System.out.printf("%d + %d = %d", firstOperand, secondOperand, firstOperand + secondOperand);
+	                break;
+	            }
+	            case '-': {
+	                System.out.printf("%d - %d = %d", firstOperand, secondOperand, firstOperand - secondOperand);
+	                break;
+	            }
+	            default: {
+	                System.out.println("Incorrect operation value passed! Calculation aborted!");
+	            }
+	        }
+	    }
+	}
